187f555eee750d5cca38c22cd69074554fd2f0fe
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-06-18 01:12:01 +0300

    done

--- JS1-2/js/hw1-2-2.js
+++ JS1-2/js/hw1-2-2.js
@@ -2,13 +2,13 @@ var NAMES_NUMBER = 5;
 var FLAG = true;
 var namesArr = [];
 
-for (var i = 1; i <= NAMES_NUMBER; i++) {
-  namesArr[i] = prompt('Имя №' + i);
+for (var i = 1; i < NAMES_NUMBER; i++) {
+  namesArr[i] = prompt('Имя №' + (i + 1));
 }
 
 var userName = prompt('Введите имя пользователя');
 
-for (var i = 0; i <= NAMES_NUMBER; i++) {
+for (var i = 0; i < NAMES_NUMBER; i++) {
   if (userName === namesArr[i]) {
     alert(userName + ', вы успешно вошли.');
     FLAG = false;

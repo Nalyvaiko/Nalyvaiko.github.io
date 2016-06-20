a2fea7549298e20488081d99aa2ef41584076091
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-06-20 22:02:38 +0300

    added check empty string

--- JS1-2/js/hw1-2-2.js
+++ JS1-2/js/hw1-2-2.js
@@ -1,19 +1,39 @@
-var NAMES_NUMBER = 5;
-var FLAG = true;
-var namesArr = [];
-
-for (var i = 1; i < NAMES_NUMBER; i++) {
-  namesArr[i] = prompt('Имя №' + (i + 1));
+function checkEmptyString(str) {
+  if (str === '' || str === null || +str === 0 || str === undefined) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-var userName = prompt('Введите имя пользователя');
+var NAMES_NUMBER = 5;
+var flag = true;
+var empt = true;
+var namesArr = [];
+
+for (var i = 1; i <= NAMES_NUMBER; i++) {
+  var name = prompt('Имя №' + i);
+
+  if (checkEmptyString(name)) {
+    alert('Пустая строка!!! Введите имя.');
+    i--;
+  } else {
+    namesArr[i - 1] = name;
+  }
+
+}
+
+var userName;
+while (checkEmptyString(userName)) {
+  var userName = prompt('Введите имя пользователя');
+}
 
 for (var i = 0; i < NAMES_NUMBER; i++) {
   if (userName === namesArr[i]) {
     alert(userName + ', вы успешно вошли.');
-    FLAG = false;
+    flag = false;
     break;
   }
 }
 
-if (FLAG) alert('Имя не найдено!');
+if (flag) alert('Имя не найдено!');

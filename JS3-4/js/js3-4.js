53c218a66fc426a68f7ecaa9d205e6e65c85f525
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-07-07 02:35:33 +0300

    js4 done with lists

--- JS3-4/js/js3-4.js
+++ JS3-4/js/js3-4.js
@@ -1,4 +1,5 @@
 var genPage = {
+  flag: true,
   pageBody: document.body,
 
   // Object of consts
@@ -8,15 +9,73 @@ var genPage = {
     pageAnswers: [["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"], ["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"], ["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"]]
   },
 
-  elementTag: ["h1", "ul", "li", "input"],
+  elementTag: ["h1", "ol", "li", "input", "ul"],
 
   // Генерация заголовка
-  genTitle: function() {
-    var titleText = this.pageConst.pageTitle;
-    var titleTag = this.elementTag[0];
+  generate: function() {
+    if (this.flag) {
+      var titleText = this.pageConst.pageTitle;
+      var titleTag = this.elementTag[0];
 
-    var title = document.createElement(titleTag);
-    title.appendChild(document.createTextNode(titleText));
-    this.pageBody.appendChild(title);
+      var title = document.createElement(titleTag);
+      title.style.textAlign = "center";
+      title.appendChild(document.createTextNode(titleText));
+      this.pageBody.appendChild(title);
+
+      // Генерация списка
+      var ol = this.elementTag[1];
+      var li = this.elementTag[2];
+      var input = this.elementTag[3];
+      var ul = this.elementTag[4];
+
+      var questionArr = this.pageConst.pageQuestion;
+      var answerArr = this.pageConst.pageAnswers;
+
+      var list = document.createElement(ol);
+        for (var i = 0; i < questionArr.length; i++) {
+
+            // Создать вопрос
+            var elemLi = document.createElement(li);
+            elemLi.innerHTML = questionArr[i];
+
+            var elemUl = document.createElement(ul);
+            elemUl.style.padding = "0";
+            elemLi.appendChild(elemUl);
+
+            // Ответы на вопрос
+            for (var j = 0; j < answerArr[i].length; j++) {
+              var elemLiChild = document.createElement(li);
+              elemLiChild.style.listStyle = "none";
+
+              var answer = document.createElement(input);
+              answer.setAttribute("type", "checkbox");
+
+              elemLiChild.appendChild(answer);
+              elemLiChild.appendChild(document.createTextNode(answerArr[i][j]));
+              elemUl.appendChild(elemLiChild);
+            }
+
+          list.appendChild(elemLi);
+        }
+      this.pageBody.appendChild(list);
+
+
+      // Генерация кнопки
+      var input = this.elementTag[3];
+      var button = document.createElement(input);
+      button.setAttribute("type", "submit");
+      button.setAttribute("value", "Проверить мои результаты");
+      button.style.margin = "10px 0 0 35%";
+      button.style.border = "1px solid black";
+      button.style.height = "31px";
+      button.style.width = "201px";
+      this.pageBody.appendChild(button);
+
+      this.flag = false;
+    } else {
+      console.log("--------------------------");
+      console.log("--- Page already exist ---");
+      console.log("--------------------------");
+    }
   }
 }

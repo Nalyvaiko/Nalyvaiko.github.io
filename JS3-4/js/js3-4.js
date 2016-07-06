a821bee492369c36624cf4af45ec0afa49d64331
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-07-06 22:17:00 +0300

    js4 added generating title

--- JS3-4/js/js3-4.js
+++ JS3-4/js/js3-4.js
@@ -1,7 +1,22 @@
-var obj, method;
+var genPage = {
+  pageBody: document.body,
 
-obj = {
-  go: function() { alert(this); }
-};
+  // Object of consts
+  pageConst: {
+    pageTitle: "Тест по программированию",
+    pageQuestion: ["Вопрос №1", "Вопрос №2", "Вопрос №3"],
+    pageAnswers: [["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"], ["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"], ["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"]]
+  },
 
-obj.go();
+  elementTag: ["h1", "ul", "li", "input"],
+
+  // Генерация заголовка
+  genTitle: function() {
+    var titleText = this.pageConst.pageTitle;
+    var titleTag = this.elementTag[0];
+
+    var title = document.createElement(titleTag);
+    title.appendChild(document.createTextNode(titleText));
+    this.pageBody.appendChild(title);
+  }
+}

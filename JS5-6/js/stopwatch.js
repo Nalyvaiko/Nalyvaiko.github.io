25a5c5bb5fd6e73912daf0d3b85445bb149ebc05
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-07-16 13:39:12 +0300

    js6

--- JS5-6/js/stopwatch.js
+++ JS5-6/js/stopwatch.js
@@ -1,15 +1,24 @@
-var mscounter = 0
+var mscounter = 0;
 var timerId;
 var start = false;
+var time;
+var splitcount = 0;
+var startcount = 0;
+var startTime;
+var stopTime;
 
-var ONE_SEC = 1000;
-var ONE_MIN = 60000;
-var ONE_HOUR = 3600000;
+var ONE_SEC = 100;
+var ONE_MIN = 6000;
+var ONE_HOUR = 360000;
 
 var hours, minutes, seconds, miliseconds;
 
 startstopbutton.addEventListener("click", startstop);
 resetbutton.addEventListener("click", reset);
+splitbutton.addEventListener("click", dosplit);
+
+var disp = document.getElementById('disp');
+var split = document.getElementById('split');
 
 
 function msToTime(duration) {
@@ -39,42 +48,113 @@ function msToTime(duration) {
   minutes = (minutes < 10) ? "0" + minutes : minutes;
   seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-  if (duration < 100) {
-    if (duration < 10) {
-      miliseconds = "00" + duration;
+  miliseconds = duration * 10;
+  if (miliseconds < 100) {
+    if (miliseconds < 10) {
+      miliseconds = "00" + miliseconds;
     } else {
-      miliseconds = "0" + duration;
+      miliseconds = "0" + miliseconds;
     }
+  }
+
+  return hours + ":" + minutes + ":" + seconds + "." + miliseconds;
+}
+
+var ONE_SEC1 = 1000;
+var ONE_MIN1 = 60000;
+var ONE_HOUR1 = 3600000;
+
+function msToTime1(duration) {
+
+  if (duration < ONE_HOUR1) {
+    hours = "0";
   } else {
-    miliseconds = duration;
+    hours = Math.floor(duration / ONE_HOUR1);
+    duration = duration % ONE_HOUR1;
+  }
+
+  if (duration < ONE_MIN1) {
+    minutes = "0";
+  } else {
+    minutes = Math.floor(duration / ONE_MIN1);
+    duration = duration % ONE_MIN1;
+  }
+
+  if (duration < ONE_SEC1) {
+    seconds = "0";
+  } else {
+    seconds = Math.floor(duration / ONE_SEC1);
+    duration = duration % ONE_SEC1;
+  }
+
+  hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+  miliseconds = duration;
+  if (miliseconds < 100) {
+    if (miliseconds < 10) {
+      miliseconds = "00" + miliseconds;
+    } else {
+      miliseconds = "0" + miliseconds;
+    }
   }
 
   return hours + ":" + minutes + ":" + seconds + "." + miliseconds;
 }
 
 function startstop() {
-
+  startcount++;
   if (!start) {
-    timerId = setInterval(function() {
-      mscounter++;
-      disp.value = msToTime(mscounter);
-    }, 1);
 
     start = true;
+    startTime = new Date();
+
+    startstopbutton.value = 'Stop';
+    timerId = window.setInterval(function() {
+        mscounter++;
+        time = msToTime(mscounter);
+        disp.value = time;
+    }, 10);
 
   } else {
-    clearInterval(timerId);
+    splitcount++;
     start = false;
+    startstopbutton.value = 'Start';
+    stopTime = new Date();
+    var dif = stopTime.getTime() - startTime.getTime();
+    time = msToTime1(dif);
+    split.innerHTML = split.innerHTML+ (splitcount + ' Stop: ' + time) + "<br>"
+    clearInterval(timerId);
   }
-}
 
+}
 
 function reset() {
   start = false;
+  mscounter = 0;
+  splitcount = 0;
+  startcount = 0;
   clearInterval(timerId);
+  startstopbutton.value = 'Start';
   disp.value = "00:00:00.000";
+  split.innerHTML = '';
 }
 
 function dosplit() {
+  if ((startcount != 0) && (startcount % 2 != 0)) {
+    splitcount++;
 
+    var timer;
+
+    stopTime = new Date();
+    var dif = stopTime.getTime() - startTime.getTime();
+    timer = msToTime1(dif);
+
+    if (startcount > 1) {
+      split.innerHTML = split.innerHTML+ (splitcount + ' Split: ' + timer) + "<br>" ;
+    } else {
+      split.innerHTML = split.innerHTML+ (splitcount + ' Split: ' + timer) + "<br>" ;
+    }
+  }
 }

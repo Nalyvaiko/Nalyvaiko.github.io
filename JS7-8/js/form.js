a3f643882b7555573739ba63d6e41a492715a459
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-07-23 17:40:12 +0300

    done JS8-2

---
+++ JS7-8/js/form.js
@@ -0,0 +1,24 @@
+$(function() {
+
+  $tooltipElement = $('<p class="tooltip"></p>');
+
+  $('input').hover(function() {
+      var tooltipText = $(this).attr('label');
+      $tooltipElement
+          .text(tooltipText)
+          .insertAfter($(this))
+          .fadeIn('slow');
+
+      },
+      function() {
+          $('.tooltip').remove();
+  });
+
+  $('#btnHelp').on('click', function() {
+      $('input').each(function(i) {
+          var tooltipText = $(this).attr('label');
+          $(this).after('<p class="tooltip">'+tooltipText+'</p>');
+      });
+  });
+
+});

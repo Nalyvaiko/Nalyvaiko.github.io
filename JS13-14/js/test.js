8d53d45212ba3ea7dcc1174ada211e55bdb0b25a
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-08-08 18:45:04 +0300

    added modal window

--- JS13-14/js/test.js
+++ JS13-14/js/test.js
@@ -2,33 +2,86 @@
 
 $(function() {
 
+    var $body = $('body');
 
-  // Template
-  var $tmpl = $('#test-template').html();
+    // Template
+    var $tmpl = $('#test-template').html();
 
-  // Data
-  var $data = {
-    title: "Тест по программированию",
-    questions: ["Президент США", "Вопрос №2", "Вопрос №3"],
-    answers: [[{ proper: true, answer: "Барак Обама"}, { proper: false, answer: "Хилари Клинтон"}, { proper: false, answer: "Дональд Трамп"}],
-    [{ proper: false, answer: "Вариант ответа №1"}, { proper: true, answer: "Вариант ответа №2"}, { proper: false, answer: "Вариант ответа №3"}],
-    [{ proper: true, answer: "Вариант ответа №1"}, { proper: false, answer: "Вариант ответа №2"}, { proper: false, answer: "Вариант ответа №3"}]]
-  };
+    // Data
+    var $data = {
+        title: "Тест по программированию",
+        questions: ["Вопрос №1: (function() { return typeof arguments; })();", "Вопрос №2: var y = 1, x = y = typeof x; x;", "Вопрос №3: (function f(f) { return typeof f(); })(function(){ return 1; });"],
+        answers: [[{ proper: true, answer: "object"}, { proper: false, answer: "array"}, { proper: false, answer: "undefined"}],
+        [{ proper: false, answer: "1"}, { proper: true, answer: "\"undefined\""}, { proper: false, answer: "\"number\""}],
+        [{ proper: true, answer: "\"number\""}, { proper: false, answer: "\"undefined\""}, { proper: false, answer: "Error"}]]
+    };
 
+    // Write to the local storage
+    localStorage.setItem('testData', JSON.stringify($data));
+    // Get object from the local storage
+    var $data = JSON.parse(localStorage.getItem('testData'));
 
-  // Write to the local storage
-  localStorage.setItem('testData', JSON.stringify($data));
-  // Get object from the local storage
-  var $data = JSON.parse(localStorage.getItem('testData'));
+    // Render
+    var $tmpl = _.template($tmpl);
+    // Append Result
+    $body.append($tmpl($data));
 
+    $('#submit').on('click', checkAnswers);
 
-  // Render
-  var $tmpl = _.template($tmpl);
-  // Append Result
-  $('body').append($tmpl($data));
+    var messagePassed = 'Тест успешно пройден!'
+    var messageNotPassed = 'Не правильно отвеченные вопросы: ';
+    var notPassed = false;
+    // Check answers
+    function checkAnswers() {
+      var $res = $('li ul');
 
-  function checkAnswers() {
-    
-  };
+      $.each($res, function(i, question) {
+        var $radioArray = $(question).find("input[type=radio]"); // 3 Arrays of radio in each()
+        for ( var j = 0; j < $data.answers[i].length; j++ ) {
+          if ($radioArray[j].checked !== $data.answers[i][j].proper) {
+            notPassed = true;
+          }
+        }
 
+        if (notPassed) {
+          messageNotPassed += ((i + 1) + ' ');
+        }
+      });
+
+      $(this).addClass('pushed');
+      if (notPassed) {
+        showModal(messageNotPassed);
+      } else {
+        showModal(messagePassed);
+      }
+    };
+
+    // Модальное окно
+    function showModal(message) {
+      $('#overlay').fadeIn(400, function() { // пoсле выпoлнения предъидущей aнимaции
+				$('#modal_form')
+          .text(message)
+					.css('display', 'block') // убирaем у мoдaльнoгo oкнa display: none;
+					.animate({opacity: 1, top: '50%'}, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
+		  });
+    }
+
+      /* Зaкрытие мoдaльнoгo oкнa, тут делaем тo же сaмoе нo в oбрaтнoм пoрядке */
+    	$('#overlay').click( function(){ // лoвим клик пo крестику или пoдлoжке
+    		$('#modal_form')
+    			.animate({opacity: 0, top: '45%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+    				function() { // пoсле aнимaции
+    					$(this).css('display', 'none'); // делaем ему display: none;
+    					$('#overlay').fadeOut(400); // скрывaем пoдлoжку
+    				}
+    			);
+
+        $('.pushed').removeClass('pushed');
+        if (notPassed) {
+          $.each( $("input[type=radio]"), function() {
+            this.checked = false;
+          });
+        }
+
+    	});
 });

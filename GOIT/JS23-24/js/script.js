1b9242d9ca53f1e713fe1f1bc65f1faa1577d2fb
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-09-08 16:55:09 +0300

    done src todo list

--- GOIT/JS23-24/js/script.js
+++ GOIT/JS23-24/js/script.js
@@ -14,11 +14,17 @@ function Model(data) {
 
   self.removeItem = function(item) {
     var index = self.data.listArr.indexOf(item);
-    
+
     if (index === -1) return;
     self.data.listArr.splice(index, 1);
 
     return self.data;
+  };
+
+  self.updateItem = function(id, item) {
+    if (item.length === 0) return;
+
+    self.data.listArr[id] = item;
   };
 }
 
@@ -37,18 +43,21 @@ function View(model) {
 
     self.elements = {
       input: $('.item-value'),
-      addBtn: $('.item-add'),
       listContainer: $('.item-list')
     };
 
     self.renderList(model.data);
-  }
+  };
+
+  self.completeItem = function(item) {
+    item.siblings('input').toggleClass('completed');
+  };
 
   self.renderList = function(data) {
     var $list = $('#list-template').html();
     var $tmpl = _.template($list);
     self.elements.listContainer.html($tmpl(data));
-  }
+  };
 
   init();
 }
@@ -57,15 +66,33 @@ function View(model) {
 function Controller(model, view) {
   var self = this;
 
-  view.elements.addBtn.on('click', addItem);
+  view.elements.input.on('keypress', addItem);
   view.elements.listContainer.on('click', '.item-delete', removeItem);
+  view.elements.listContainer.on('click', '.item-complete', completeItem);
+  view.elements.listContainer.on('change', '.item', updateItem);
 
-  function addItem() {
-    var newItem = view.elements.input.val();
+  function completeItem() {
+    var item = $(this);
 
-    model.addItem(newItem);
+    view.completeItem(item);
+  }
+
+  function updateItem() {
+    var item = $(this).val();
+    var id =  $(this).attr('id');
+
+    model.updateItem(id, item);
     view.renderList(model.data);
-    view.elements.input.val('');
+  }
+
+  function addItem(event) {
+    if (event.which === 13) {
+      var newItem = view.elements.input.val();
+
+      model.addItem(newItem);
+      view.renderList(model.data);
+      view.elements.input.val('');
+    }
   }
 
   function removeItem() {
@@ -78,8 +105,7 @@ function Controller(model, view) {
 
 // Init
 $(function() {
-  // var firstToDoList = ['learn javascript', 'learn react', 'make coffee'];
-  var firstToDoList = {listArr: ['learn javascript', 'learn react', 'make coffee']};
+  var firstToDoList = {listArr: ['learn javascript', 'buy new robbe', 'make coffee']};
   var model = new Model(firstToDoList);
   var view = new View(model);
   var controller = new Controller(model, view);

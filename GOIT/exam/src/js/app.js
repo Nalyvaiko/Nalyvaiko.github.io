2d6fe751c0f6781656aeaf89dbc2edf17ea06b8c
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-09-17 11:27:09 +0300

    done partners added ideas

--- GOIT/exam/src/js/app.js
+++ GOIT/exam/src/js/app.js
@@ -39,4 +39,43 @@ $(function() {
     $('.carousel--step3').jcarousel('scroll', '-=1');
   });
 
+  const defKeywords = 'sport health extreme games culture relaxation travelling';
+
+  let keywords = $('#searchInput').val().length > 0 ? $('#searchInput').val() : defKeywords;
+
+  $.ajax({
+    // async: false,
+    url: 'https://api.datamarket.azure.com/Bing/Search/v1/Composite?Sources=%27Image%27&$format=json&$top=15&$skip=15&Query=%27'+ keywords + '%27',
+    headers: {
+      'Authorization': 'Basic ' + btoa('MbfQyJYgDJkVhKwyyV/dJqFoNrxhauqxCSa7+/udzmU=:MbfQyJYgDJkVhKwyyV/dJqFoNrxhauqxCSa7+/udzmU=')
+  	},
+    success: function(json) {
+      let data = json.d.results[0];
+      console.log(data);
+      renderList(data);
+      loadMasonry();
+    }
+  });
+
+  function loadMasonry() {
+    let $gallery = $('.my-gallery-class');
+
+    $gallery.imagesLoaded(function() {
+      $gallery.masonry({
+        itemSelector : '.grid-item',
+        transitionDuration: 500,
+        percentPosition: true,
+        fitWidth: true,
+        gutter: 10
+      })
+    })
+  };
+
+  function renderList(data) {
+    let $list = $('#list');
+    let $searchTmpl = $('#search-template').html();
+    let $tmpl = _.template($searchTmpl);
+    $list.html($tmpl(data));
+  };
+
 });

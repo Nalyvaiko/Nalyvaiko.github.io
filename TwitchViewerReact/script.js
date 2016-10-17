f157b88e79f6da93765ff314117d10dcf5f5e017
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-10-18 00:27:11 +0300

    added recur request

--- TwitchViewerReact/script.js
+++ TwitchViewerReact/script.js
@@ -1,18 +1,53 @@
-var Viewer = React.createClass({
+const Channel = React.createClass({
+    render: function() {
+        return (
+            <li className="channel">
+                <img  alt="img" className="channel-image"/>
+                <div className="channel-info">
+                    <div className="channel-name"></div>
+                    <div className="stream-name"></div>
+                </div>
+            </li>
+        );
+    }
+});
+
+const Viewer = React.createClass({
     getInitialState: function() {
         return {
-            tab: "all"
+            channels: ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"],
+            channelsInfo: [],
+            display: 'all'
         }
     },
 
     loadData: function() {
-        fetch('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?')
-            .then(function(response) {
-                console.log(response);
-            })
-            .catch(function(ex) {
-                console.log('Error: ', ex);
-            })
+        const that = this;
+        let channelsList = that.state.channels;
+        let count = channelsList.length;
+        const client_id = 'bdrnkzccnzobyx9cmoqjcwei7whumvz';
+
+        function request(count, list) {
+            $.ajax({
+                async: true,
+                url: 'https://api.twitch.tv/kraken/streams/' + list[count] + '?client_id=' + client_id + '&callback=?',
+                type: 'GET',
+                dataType: 'json',
+                success: function(data) {
+                    if (count >= 0) {
+                        count--;
+                        let arr = that.state.channelsInfo;
+                        arr.push(data);
+                        that.setState({
+                            channelsInfo: arr
+                        });
+                        request(count, list);
+                    }
+                }
+            });
+        };
+        
+        request(count - 1, channelsList);
     },
 
     componentDidMount: function() {
@@ -22,12 +57,19 @@ var Viewer = React.createClass({
     render: function() {
         return (
             <div>
-                <p>Hello!</p>
+                <div className="display">
+                    <div className="all"></div>
+                    <div className="online"></div>
+                    <div className="ofline"></div>
+                </div>
+                <input type="text" className="search-field"/>
+                <ul className="streamers-list">
+                    <Channel />
+                </ul>
             </div>
         );
     }
 });
-
 
 ReactDOM.render(
     <Viewer />,

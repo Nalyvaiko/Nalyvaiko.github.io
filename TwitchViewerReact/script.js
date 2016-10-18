29e0a14353c97fbceddfa1a1c2fe56bb16d15701
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-10-18 18:06:14 +0300

    basic twitchviewer

--- TwitchViewerReact/script.js
+++ TwitchViewerReact/script.js
@@ -2,10 +2,13 @@ const Channel = React.createClass({
     render: function() {
         return (
             <li className="channel">
-                <img  alt="img" className="channel-image"/>
+                <a href={this.props.href} className="channel-link" target="_blank">
+                    <img src={this.props.image} alt="img" className="channel-image" width="60px" height="60px" />
+                </a>
                 <div className="channel-info">
-                    <div className="channel-name"></div>
-                    <div className="stream-name"></div>
+                    <div className="channel-name">{this.props.name}</div>
+                    <div className="channel-game">{this.props.game}</div>
+                    <div className="stream-name">{this.props.stream}</div>
                 </div>
             </li>
         );
@@ -17,7 +20,7 @@ const Viewer = React.createClass({
         return {
             channels: ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"],
             channelsInfo: [],
-            display: 'all'
+            channelsDisplay: []
         }
     },
 
@@ -34,19 +37,21 @@ const Viewer = React.createClass({
                 type: 'GET',
                 dataType: 'json',
                 success: function(data) {
+                    data.name = list[count];
                     if (count >= 0) {
                         count--;
                         let arr = that.state.channelsInfo;
                         arr.push(data);
                         that.setState({
-                            channelsInfo: arr
+                            channelsInfo: arr,
+                            channelsDisplay: arr
                         });
                         request(count, list);
                     }
                 }
             });
         };
-        
+
         request(count - 1, channelsList);
     },
 
@@ -54,17 +59,70 @@ const Viewer = React.createClass({
         this.loadData();
     },
 
+    handleSearch: function(e) {
+        let searchQuery = e.target.value.toLowerCase();
+
+        let channelsDisplay = this.state.channelsInfo.filter(function(channel) {
+            let searchValue = channel.name.toLowerCase();
+            return searchValue.indexOf(searchQuery) !== -1;
+        });
+
+        this.setState({
+            channelsDisplay: channelsDisplay
+        });
+    },
+
+    handleDisplay: function(e) {
+        let display = e.target.classList.value;
+        switch(display) {
+            case "all":
+                let channelsAll = this.state.channelsInfo;
+                this.setState({
+                    channelsDisplay: channelsAll
+                });
+                break;
+            case "online":
+                let channelsOnline = this.state.channelsInfo.filter(function(channel) {
+                    return channel.stream;
+                });
+                this.setState({
+                    channelsDisplay: channelsOnline
+                });
+                break;
+            case "offline":
+                let channelsOffline = this.state.channelsInfo.filter(function(channel) {
+                    return !channel.stream;
+                });
+                this.setState({
+                    channelsDisplay: channelsOffline
+                });
+                break;
+        }
+    },
+
     render: function() {
         return (
-            <div>
+            <div className="channels">
                 <div className="display">
-                    <div className="all"></div>
-                    <div className="online"></div>
-                    <div className="ofline"></div>
+                    <div className="all" onClick={this.handleDisplay}>All</div>
+                    <div className="online" onClick={this.handleDisplay}>OnLine</div>
+                    <div className="offline" onClick={this.handleDisplay}>OffLine</div>
                 </div>
-                <input type="text" className="search-field"/>
-                <ul className="streamers-list">
-                    <Channel />
+                <input type="text" className="search-field" onChange={this.handleSearch} />
+                <ul className="channels-list">
+                    {
+                        this.state.channelsDisplay.map(function(el, i) {
+                            return <Channel
+                                    key={i}
+                                    image={el.stream ? el.stream.channel.logo : "http://www.sd-gaming.com/templates/sd-gaming-eventpage/images/twitch-offline.png"}
+                                    href={el.stream ? el.stream.channel.url : "http://www.twitch.tv/" + el.name}
+                                    name={el.name}
+                                    game={el.stream ? el.stream.channel.game : null}
+                                    stream={el.stream ? el.stream.channel.status : "offline"}
+                                    />
+                        })
+                    }
+
                 </ul>
             </div>
         );

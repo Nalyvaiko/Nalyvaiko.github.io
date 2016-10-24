7cd4e91200060ffbbf8e982170677970778a3c63
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-10-24 19:22:26 +0300

    done

--- GOIT/Markup/Biruang/js/script.js
+++ GOIT/Markup/Biruang/js/script.js
@@ -4,4 +4,14 @@ $(function() {
         speed: 850,
         arrows: false
     });
+
+    function setEqualHeight(columns) {
+		var tallestcolumn = 0;
+		columns.each( function() {
+			currentHeight = $(this).height();
+			if(currentHeight > tallestcolumn) { tallestcolumn = currentHeight; } } );
+			columns.height(tallestcolumn);
+	}
+
+	setEqualHeight($(".description-col"));
 });
